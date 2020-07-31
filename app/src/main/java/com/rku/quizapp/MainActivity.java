6965657f1642d5de5cb5eb3c04843592a464ae60
darshit <drajyaguru698@rku.ac.in>
2020-07-31 19:59:18 +0530

    Animation Added.

--- app/src/main/java/com/rku/quizapp/MainActivity.java
+++ app/src/main/java/com/rku/quizapp/MainActivity.java
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    //Variable
+    public Animation topAnim;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this.R.anim.top_animation);
+
+        //Hooks
+        image = findViewById(this.R.id.ImageView);
+
+        image.setAnimation(topAnim);
     }
 }
