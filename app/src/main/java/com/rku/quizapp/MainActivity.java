422835e1fc3f9e9055bac3de9559432a6bf22f69
darshit <drajyaguru698@rku.ac.in>
2020-08-02 13:29:04 +0530

    Login Page Added.

--- app/src/main/java/com/rku/quizapp/MainActivity.java
+++ app/src/main/java/com/rku/quizapp/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static com.rku.quizapp.R.anim.mixed_anim;
 import static com.rku.quizapp.R.anim.righttoleft;
 import static com.rku.quizapp.R.anim.rotate;
@@ -16,22 +19,24 @@ import static com.rku.quizapp.R.anim.zoomin;
 import static com.rku.quizapp.R.anim.zoomout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     //Animation
     //Variable
     Animation topAnim;
     ImageView IM;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         IM = (ImageView)findViewById(R.id.ImgLogo);
         //Animation
         Animation animation = AnimationUtils.loadAnimation(this,rotate );
         IM.startAnimation(animation);
+
+
+
     }
 }
