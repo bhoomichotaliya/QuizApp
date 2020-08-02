68396fb1958f676439510ade287afde64851b503
darshit <drajyaguru698@rku.ac.in>
2020-08-02 14:18:53 +0530

    Login Page Added and Animation Added on Button.

--- app/src/main/java/com/rku/quizapp/login.java
+++ app/src/main/java/com/rku/quizapp/login.java
@@ -3,12 +3,31 @@ package com.rku.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static com.rku.quizapp.R.anim.bounce;
+import static com.rku.quizapp.R.anim.rotate;
 
 public class login extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        btn = (Button) findViewById(R.id.btnLogin);
+        //Animation
+        Animation animation = AnimationUtils.loadAnimation(this,bounce);
+        btn.startAnimation(animation);
+
+        btn = (Button) findViewById(R.id.btnRegister);
+        //Animation
+        Animation anim = AnimationUtils.loadAnimation(this,bounce);
+        btn.startAnimation(anim);
     }
 }
