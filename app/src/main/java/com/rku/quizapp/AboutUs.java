32014ae7b038ca3e8bbde97766f4e5bd5bf556b6
darshit <drajyaguru698@rku.ac.in>
2020-08-03 17:48:56 +0530

    About Us page Added.

--- app/src/main/java/com/rku/quizapp/AboutUs.java
+++ app/src/main/java/com/rku/quizapp/AboutUs.java
@@ -2,17 +2,13 @@ package com.rku.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class contact_us extends AppCompatActivity {
-    private Button btn;
+public class AboutUs extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_us);
-
+        setContentView(R.layout.activity_about_us);
     }
 }
